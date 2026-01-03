d1f3d8970f374a45c56cdfb68725c94580829db7
SRKIM <routenew50@gmail.com>
2026-01-03 14:03:15 +0900

    Force deploy: ignore typescript errors

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // TypeScript 에러가 있어도 빌드를 강제로 진행하도록 설정
   typescript: {
     ignoreBuildErrors: true,
   },
-  // ESLint 에러가 있어도 빌드를 강제로 진행하도록 설정
+  // ESLint 에러(코드 스타일)가 있어도 빌드를 강제로 진행하도록 설정
   eslint: {
     ignoreDuringBuilds: true,
   },
